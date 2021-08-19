ae7038b983180ae569d8ab1853fb109bcfe62552
vidhikothari2007 <75124794+vidhikothari2007@users.noreply.github.com>
2021-08-19 17:14:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+var database;
+var position;
+var gameState=0;
+var playerCount=0;
+var form, player, game; 
+
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game= new Game();
+  game.getState();
+  game.start(); 
+}
+
+function draw(){
+  background("white");
+  
+    drawSprites();
+  
+}
+
+
